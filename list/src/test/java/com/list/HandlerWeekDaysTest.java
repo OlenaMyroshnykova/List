dd85411277c7e59fc43a3ce1587390446e626d6a
OlenaMyroshnykova <elenamirespania@gmail.com>
2024-11-10 23:24:27 +0100

    fixes

--- list/src/test/java/com/list/HandlerWeekDaysTest.java
+++ list/src/test/java/com/list/HandlerWeekDaysTest.java
@@ -1,7 +1,10 @@
 package com.list;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.hasItem;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
 
 import org.junit.jupiter.api.Test;
 
@@ -21,4 +24,39 @@ public class HandlerWeekDaysTest {
         assertThat(handlerWeekDays.getDays().get(0), is("Lunes"));
     }
 
-}
+    @Test
+    void testRemoveDay() {
+        HandlerWeekDays manager = new HandlerWeekDays();
+        boolean removed = manager.removeDay("Lunes");
+        assertThat(removed, is(true));
+        assertThat(manager.getDays(), not(hasItem("Lunes")));
+    }
+
+    @Test
+    void testGetDay() {
+        HandlerWeekDays manager = new HandlerWeekDays();
+        String day = manager.getDay(0);
+        assertThat(day, is("Lunes"));
+    }
+
+    @Test
+    void testDayExists() {
+        HandlerWeekDays manager = new HandlerWeekDays();
+        assertThat(manager.dayExists("Martes"), is(true));
+        assertThat(manager.dayExists("Funday"), is(false));
+    }
+
+    @Test
+    void testSortDaysAlphabetically() {
+        HandlerWeekDays manager = new HandlerWeekDays();
+        manager.sortDaysAlphabetically();
+        assertThat(manager.getDays().get(0), is("Domingo"));
+        assertThat(manager.getDays().get(6), is("Viernes"));
+    }
+
+    @Test
+    void testClearDays() {
+        HandlerWeekDays manager = new HandlerWeekDays();
+        manager.clearDays();
+        assertThat(manager.getDays(), is(empty()));
+    }}
